a21949edbf98d2ae313bc3c068dd90925a11c92b
Zack Salama <zacksalama@gmail.com>
2025-05-04 21:33:31 -0400

    cta init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,85 @@
-export default function page() {
-    return <div className='center'>heyo</div>
+// components/Hero.tsx
+'use client'
+import React, { useState, useEffect } from 'react'
+import { motion, AnimatePresence, Variants } from 'framer-motion'
+
+const words = ['Website', 'Store', 'Domain']
+
+// Variants for the container of letters
+const containerVariants: Variants = {
+    initial: {},
+    animate: {
+        transition: {
+            staggerChildren: 0.1,
+            when: 'beforeChildren',
+        },
+    },
+    exit: {
+        transition: {
+            staggerChildren: 0.1,
+            staggerDirection: -1, // reverse order
+            when: 'afterChildren',
+        },
+    },
+}
+
+// Variants for each letter
+const letterVariants: Variants = {
+    initial: { opacity: 0, y: 20 },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: { type: 'spring', stiffness: 300 },
+    },
+    exit: { opacity: 0, y: -20, transition: { duration: 0.2 } },
+}
+
+export default function Hero() {
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((i) => (i + 1) % words.length)
+        }, 4000) // 4s per word
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <section
+            className='
+      bg-gray-50 flex items-center justify-start
+      h-[calc(100vh-5.5rem)] sm:h-[calc(100vh-4.5rem)]
+      px-6 md:px-12 lg:px-24
+    '
+        >
+            <div className='max-w-2xl ml-8 md:ml-16 lg:ml-[20%]'>
+                <h1 className='text-5xl md:text-7xl lg:text-8xl font-medium leading-tight text-gray-900'>
+                    Design your
+                    <br />
+                    <AnimatePresence mode='wait'>
+                        <motion.span
+                            key={words[index]}
+                            className='inline-block'
+                            variants={containerVariants}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                        >
+                            {words[index].split('').map((char, i) => (
+                                <motion.span
+                                    key={i}
+                                    className='inline-block'
+                                    variants={letterVariants}
+                                >
+                                    {char}
+                                </motion.span>
+                            ))}
+                        </motion.span>
+                    </AnimatePresence>
+                    <br />
+                    Today
+                </h1>
+            </div>
+        </section>
+    )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,6 +9,7 @@ import { Menu, X } from 'lucide-react'
 
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false)
+    const [isMounted, setIsMounted] = useState(false)
     const pathname = usePathname()
 
     const links = [
@@ -26,8 +27,99 @@ export default function Navbar() {
         document.body.style.overflow = menuOpen ? 'hidden' : 'auto'
     }, [menuOpen])
 
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    // return skeleton of nav is not mounted yet
+    // This is to prevent flickering of the navbar on initial load
+    // try to keep styling consistent with the mounted version
+    if (!isMounted)
+        return (
+            <nav className='sticky top-0 z-50 shadow-md px-6 py-4'>
+                <div className='flex justify-between items-center max-w-6xl mx-auto'>
+                    {/* Logo */}
+                    <Link href='/'>
+                        <div className='flex items-center space-x-2'>
+                            <Image
+                                src='/logo.png'
+                                alt='Undermouse Logo'
+                                width={40}
+                                height={40}
+                            />
+                        </div>
+                    </Link>
+
+                    {/* Desktop Links */}
+                    <div className='hidden md:flex space-x-6'>
+                        {links.map(({ href, label }) => (
+                            <Link
+                                key={href}
+                                href={href}
+                                className={clsx(
+                                    'transition-colors duration-200 hover:text-blue-600',
+                                    {
+                                        'text-blue-600 font-semibold border-b-2 border-blue-600 pb-0.5':
+                                            pathname === href && !isHome,
+                                    }
+                                )}
+                            >
+                                {label}
+                            </Link>
+                        ))}
+                    </div>
+
+                    {/* Hamburger */}
+                    <button
+                        className='md:hidden'
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        aria-label='Toggle menu'
+                    >
+                        {menuOpen ? <X size={24} /> : <Menu size={24} />}
+                    </button>
+                </div>
+
+                {/* Sidebar overlay */}
+                <div
+                    className={clsx(
+                        'fixed inset-0 bg-gray-600 bg-opacity-40 transition-opacity duration-300 md:hidden z-40',
+                        menuOpen ? 'opacity-40 visible' : 'opacity-0 invisible'
+                    )}
+                    onClick={() => setMenuOpen(false)}
+                />
+
+                {/* Sidebar panel */}
+                <div
+                    className={clsx(
+                        'fixed top-0 right-0 h-full w-64 bg-white shadow-lg transition-transform duration-300 ease-in-out z-50 md:hidden',
+                        menuOpen ? 'translate-x-0' : 'translate-x-full'
+                    )}
+                >
+                    <div className='flex flex-col px-6 py-6 space-y-6 justify-center h-full'>
+                        {links.map(({ href, label }) => (
+                            <Link
+                                key={href}
+                                href={href}
+                                onClick={() => setMenuOpen(false)}
+                                className={clsx(
+                                    'transition-colors duration-200 hover:text-blue-600 text-center',
+                                    {
+                                        'text-blue-600 font-semibold border-b-2 border-blue-600 pb-0.5 ':
+                                            pathname === href && !isHome,
+                                    }
+                                )}
+                            >
+                                {label}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </nav>
+        )
+
+    // Render the navbar normally if mounted
     return (
-        <nav className='bg-white shadow-md px-6 py-4 relative z-50'>
+        <nav className='sticky top-0 z-50 bg-white/50 backdrop-blur-md shadow-md px-6 py-4'>
             <div className='flex justify-between items-center max-w-6xl mx-auto'>
                 {/* Logo */}
                 <Link href='/'>
@@ -86,16 +178,16 @@ export default function Navbar() {
                     menuOpen ? 'translate-x-0' : 'translate-x-full'
                 )}
             >
-                <div className='flex flex-col px-6 py-6 space-y-6'>
+                <div className='flex flex-col px-6 py-6 space-y-6 justify-center h-full'>
                     {links.map(({ href, label }) => (
                         <Link
                             key={href}
                             href={href}
                             onClick={() => setMenuOpen(false)}
                             className={clsx(
-                                'transition-colors duration-200 hover:text-blue-600',
+                                'transition-colors duration-200 hover:text-blue-600 text-center',
                                 {
-                                    'text-blue-600 font-semibold border-b-2 border-blue-600 pb-0.5':
+                                    'text-blue-600 font-semibold border-b-2 border-blue-600 pb-0.5 ':
                                         pathname === href && !isHome,
                                 }
                             )}
