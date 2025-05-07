7bb3af7410f7cff399b84e9d94d7d074a6a9601c
Zack Salama <zacksalama@gmail.com>
2025-05-07 15:43:03 -0400

    animation rework and testing

--- src/app/(hero)/layout.tsx
+++ src/app/(hero)/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 import '../globals.css'
 import styles from './layout.module.css'
-import { ParallaxProvider } from 'react-scroll-parallax'
 
 export default function RootLayout({
     children,
@@ -9,11 +8,9 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <ParallaxProvider>
-            <div className={styles.container}>
-                {children}
-                {/* <Footer /> */}
-            </div>
-        </ParallaxProvider>
+        <div className={styles.container}>
+            {children}
+            {/* <Footer /> */}
+        </div>
     )
 }
--- src/app/(hero)/page.tsx
+++ src/app/(hero)/page.tsx
@@ -2,24 +2,17 @@
 import CTA from '@/components/CTA'
 import styles from './page.module.css'
 import Portfolio from '@/components/Portfolio/Portfolio'
-// import MiddleImage from '@/components/MiddleImage/MiddleImage'
-// import ParallaxImage from '@/components/ParallaxImage/ParallaxImage'
-import { Parallax, useParallax } from 'react-scroll-parallax'
 import Image from 'next/image'
-// import MouseChillin from '@/public/mousechillin.svg'
-// import MouseChillin from '@/components/MouseChillin/MouseChillin'
-// import { useInView } from 'react-intersection-observer'
+import * as motion from 'motion/react-client'
+
+const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ff0088',
+    borderRadius: 5,
+}
 
 export default function Hero() {
-    const parallax_scroller = useParallax<HTMLDivElement>({
-        speed: 10,
-    })
-
-    // const { ref, inView } = useInView({
-    //     triggerOnce: true,
-    //     threshold: 0.1,
-    // })
-
     return (
         <>
             <section className={` ${styles.CTA_container} z-5 `}>
@@ -35,72 +28,21 @@ export default function Hero() {
                     </button>
                 </div>
             </section>
-            {/* background wallpapers */}
-            <div className={styles.mouse_art_container}>
-                <div ref={parallax_scroller.ref}>
-                    <Parallax
-                        className={styles.mousechillin_container}
-                        speed={50}
-                    >
-                        <Image
-                            src='/beach.jpg'
-                            className={styles.mousechillin}
-                            alt='Beach'
-                            width={400}
-                            height={400}
-                        />
-                    </Parallax>
-                </div>
-                <div ref={parallax_scroller.ref}>
-                    <Parallax
-                        className={styles.mousechillin_container}
-                        speed={50}
-                    >
-                        <Image
-                            src='/beach2.jpg'
-                            className={styles.mousechillin}
-                            alt='Beach'
-                            width={300}
-                            height={300}
-                        />
-                    </Parallax>
-                </div>
-                <div ref={parallax_scroller.ref}>
-                    <Parallax
-                        className={styles.mousechillin_container}
-                        speed={50}
-                    >
-                        <Image
-                            src='/beach3.jpg'
-                            className={styles.mousechillin}
-                            alt='Beach'
-                            width={500}
-                            height={300}
-                        />
-                    </Parallax>
-                </div>
-            </div>
-            {/* <div ref={parallax_scroller.ref}>
-                <Parallax
-                    // src='mousechillin.svg'
-                    // alt='beach'
-                    // height={600}
-                    // speed={0.15}
-                    // translateY={[-20, 20]}
-                    className={styles.mousechillin_container}
-                    speed={-10}
-                    // style={{ marginLeft: '15rem' }}
-                >
-                    <Image
-                        src='/mousechillin.svg'
-                        className={styles.mousechillin}
-                        alt='Beach'
-                        width={100}
-                        height={300}
-                    />
-                </Parallax>
-            </div> */}
 
+            <motion.div
+                style={box}
+                initial={{ scale: 0 }}
+                animate={{ rotate: 360, scale: 1 }}
+                transition={{ duration: 1 }}
+            />
+            <div className='w-full flex justify-center items-center'>
+                <motion.div
+                    style={box}
+                    initial={{ scale: 1 }}
+                    whileHover={{ scale: 2 }}
+                    // whileTap={{ scale: 0.9 }}
+                ></motion.div>
+            </div>
             <Portfolio />
             <Portfolio />
             <Portfolio />
